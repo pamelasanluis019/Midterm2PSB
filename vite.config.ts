e6930962d98012e5a4e3a4fcc241fa661cdf8515
pamelasanluis019 <pamelasanluis019@gmail.com>
2026-04-10 01:10:22 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Midterm2PSB/"
+  base: "/Midterm2PSB/",     // ← Este es el nombre de tu repo
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+      },
+    },
+  },
 });
